8197b61f7f6cfee2a5ed482c170ad9846b5b5d39
Karen Castigador <kvcastigador@gmail.com>
2023-09-20 04:03:33 +0000

    RWorksheet

---
+++ RWorksheet_Castigador#1.R
@@ -0,0 +1,36 @@
+#Vector
+
+ age <-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,
+51, 35, 24, 33, 41.)
+ 
+#data points
+ length (age)
+ 
+ 
+#reciprocal
+  reciprocal <-1/ age
+  reciprocal
+  
+#Assign also new age <- c (age,0,age).
+  new_age <- c(age,0,age)
+  new_age
+  
+#Sort the values for age
+  sort_age <-c(age)
+  sort_age
+  
+#Minimum and Maximum
+  min_age <-c (age)
+  min_age
+  max_age <-c (age)
+  max_age
+  
+#VectorData
+  dataV <-c (2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+             2.5, 2.3, 2.4,2.7.)
+ #data points
+  length data
+  
+  
+  
